326bc1a958df5780c5b65f4196423f830bedfa9b
Mykyta Dmytrovich <nikita.dmitrovich@gmail.com>
2026-07-06 18:26:18 +0300

    [feat] make user and source managment

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const express = require("express");
 const { google } = require("googleapis");
 const fs = require("fs");
 
+const { pool, InsertGoogleAccount, InsertSource, getCredentialsByUserId, getSourcesByYTId, deleteSource, deleteAllSources,deleteAccount } = require('./db.js')
+
 const app = express();
+app.use(express.json())
+
 const PORT = process.env.PORT || 3000;
 
 REDIRECT_URI = `http://localhost:${PORT}/oauth2callback`
@@ -15,25 +19,28 @@ const oauth2Client = new google.auth.OAuth2(
   REDIRECT_URI
 );
 
-const SCOPES = ["https://www.googleapis.com/auth/youtube.upload"];
+const SCOPES = ["https://www.googleapis.com/auth/youtube.upload",
+  "https://www.googleapis.com/auth/userinfo.email",
+  "https://www.googleapis.com/auth/userinfo.profile",
+];
 
 
 app.get("/auth", (req, res) => {
 
-    console.log("Loaded CLIENT_ID:", CLIENT_ID);
-    console.log("Loaded CLIENT_SECRET:", CLIENT_SECRET?.slice(0, 10) + "...");
+  const tg_id = req.query.tg_id;
 
   const authUrl = oauth2Client.generateAuthUrl({
-    access_type: "offline", 
-    prompt: "consent",      
+    access_type: "offline",
+    prompt: "consent",
     scope: SCOPES,
+    state: tg_id
   });
   res.redirect(authUrl);
 });
-
 app.get("/oauth2callback", async (req, res) => {
-  const { code } = req.query;
+  const { code, state: tg_id } = req.query;
 
+  console.log(tg_id)
   if (!code) {
     return res.status(400).send("Missing authorization code");
   }
@@ -42,11 +49,10 @@ app.get("/oauth2callback", async (req, res) => {
     const { tokens } = await oauth2Client.getToken(code);
     oauth2Client.setCredentials(tokens);
 
-    // tokens = { access_token, refresh_token, scope, token_type, expiry_date }
-    console.log("Received tokens:", tokens);
+    const oauth2 = google.oauth2({ version: "v2", auth: oauth2Client });
+    const { data: profile } = await oauth2.userinfo.get();
 
-    // Save to a file for now (swap this for your MySQL insert)
-    fs.writeFileSync("tokens.json", JSON.stringify(tokens, null, 2));
+    await InsertGoogleAccount(tg_id, tokens, profile)
 
     res.send("✅ Authorization successful! You can close this tab.");
   } catch (err) {
@@ -55,7 +61,65 @@ app.get("/oauth2callback", async (req, res) => {
   }
 });
 
+
+app.get("/user/:id", async (req, res) => {
+  const { id } = req.params;
+  console.log(id)
+  const data = await getCredentialsByUserId(id);
+  res.send(data)
+})
+app.delete("/user",async (req,res)=>{
+  try {
+    const {account_id,chat_id} = req.body;
+
+    await deleteAccount(account_id,chat_id)
+    
+    return res.send("Ok")
+  } catch (e) {
+    console.error(e)
+    return res.status(500).send("Fail")
+  }
+})
+
+app.get("/source/:yt_id", async (req, res) => {
+  const { yt_id } = req.params;
+
+  const data = await getSourcesByYTId(yt_id);
+
+  return res.send(data);
+})
+
+app.post("/source", async (req, res) => {
+  try {
+    const { yt_id, link } = req.body;
+
+    await InsertSource(yt_id, link);
+
+    return res.send("Ok");
+  } catch (e) {
+    console.error(e);
+    return res.status(500).send("Fail");
+  }
+})
+app.delete("/source", async (req, res) => {
+  try {
+    const { yt_id, link } = req.body;
+    if(link){
+      await deleteSource(yt_id, link)
+    }else{
+      await deleteAllSources(yt_id)
+    }
+    return res.send("Ok")
+  } catch (e) {
+    console.error(e);
+    return res.status(500).send("Fail")
+  }
+})
+
 app.listen(PORT, () => {
   console.log(`Server running: http://localhost:${PORT}`);
   console.log(`Start auth flow at: http://localhost:${PORT}/auth`);
 });
+
+
+
---
+++ db.js
@@ -0,0 +1,69 @@
+const db = require("mysql2/promise");
+
+const pool = db.createPool({
+    host: "localhost",
+    user: "mik",
+    password: "11211121",
+    database: "tgbot",
+});
+
+async function InsertGoogleAccount(user_id, tokens, profile) {
+    await pool.execute(
+        `INSERT INTO youtube_credentials
+      (user_id, access_token, refresh_token, scope, token_type, expiry_date, google_email, google_name)
+     VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
+        [
+            user_id,
+            tokens.access_token,
+            tokens.refresh_token || null,
+            tokens.scope,
+            tokens.token_type,
+            tokens.expiry_date,
+            profile.email,
+            profile.name,
+        ]
+    );
+}
+
+async function InsertSource(yt_id, link) {
+    await pool.execute(`INSERT INTO tiktok_sources (yt_id,link) 
+        VALUES (?,?)`, [yt_id, link])
+}
+async function getSourcesByYTId(yt_id) {
+    const [rows] = await pool.execute(`
+        SELECT google_email, google_name, tiktok_sources.link 
+        FROM youtube_credentials   
+        INNER JOIN tiktok_sources 
+        ON youtube_credentials.id = tiktok_sources.yt_id 
+        WHERE youtube_credentials.id=?`,[yt_id]);
+
+    return rows || null;
+}
+async function deleteSource(yt_id,link) {
+    await pool.execute(`DELETE FROM tiktok_sources WHERE link=? and yt_id=?;`,[link,yt_id])
+}
+async function deleteAllSources(yt_id) {
+    await pool.execute(`DELETE FROM tiktok_sources WHERE yt_id=?;`,[yt_id])
+}
+async function deleteAccount(account_id,chat_id){
+    await pool.execute(`DELETE FROM youtube_credentials WHERE id=? AND user_id=?;`,[account_id,chat_id])
+}
+
+async function getCredentialsByUserId(userId) {
+    const [rows] = await pool.execute(
+        "SELECT * FROM youtube_credentials WHERE user_id = ?",
+        [userId]
+    );
+    return rows || null;
+}
+
+async function getCredentialsByEmail(email) {
+    const [rows] = await pool.execute(
+        "SELECT * FROM youtube_credentials WHERE google_email = ? LIMIT 1",
+        [email]
+    );
+    return rows[0] || null;
+}
+
+
+module.exports = { pool, InsertGoogleAccount, getCredentialsByUserId, InsertSource, getSourcesByYTId, deleteSource,deleteAllSources,deleteAccount }
